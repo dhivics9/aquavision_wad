2c0e5c7704a4ead07f9ab9f5c88b8181d2e4d8d6
Azel Pandya Maheswara Nur Achmad <azelpandy2@gmail.com>
2024-12-23 08:56:32 +0700

    Sensor Update

--- app/Models/Sensor.php
+++ app/Models/Sensor.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
 
 class Sensor extends Model
 {
     //
+    protected $fillable = [
+        'sensor_id',
+        'sensor name',
+        'sensor type',
+        'sensor location',
+        'sensor status',
+        'created_at',
+        'updated_at',
+        'user_id'
+    ];
+    public function users():BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id', 'id');
+    }
+    public function waterMonitorings():HasMany
+    {
+        return $this->hasMany(WaterMonitoring::class,'sensor_id', 'id');
+    }
+    public function reports():HasMany
+    {
+        return $this->hasMany(Report::class,'sensor_id', 'id');
+    }
+
 }
