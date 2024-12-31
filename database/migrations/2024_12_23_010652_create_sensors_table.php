86085533836f94b119f4855124c64dcee046bb71
Azel Pandya Maheswara Nur Achmad <142408147+AzlNach@users.noreply.github.com>
2024-12-31 15:59:23 +0700

    Update 2024_12_23_010652_create_sensors_table.php

--- database/migrations/2024_12_23_010652_create_sensors_table.php
+++ database/migrations/2024_12_23_010652_create_sensors_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('sensors', function (Blueprint $table) {
             $table->id();
-            $table->string('sensor_name')->unique();
-            $table->string('sensor_type')->unique();
-            $table->string('sensor_location')->unique();
+            $table->string('sensor_name');
+            $table->string('sensor_type');
+            $table->string('sensor_location');
             $table->string('sensor_status')->default('active');
-            // $table->timestamps()->nullable();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-        });
+            $table->timestamps(); 
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+        });        
     }
 
     /**
